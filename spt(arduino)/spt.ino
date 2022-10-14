ee5337b56b6233d68a0d740345e22ba80617cc57
silver7i <77370836+silver7i@users.noreply.github.com>
2022-10-14 09:50:41 +0900

    Add files via upload

--- spt(arduino)/spt.ino
+++ spt(arduino)/spt.ino
@@ -3,7 +3,9 @@
 int servo = 9;
 int flag = 1; // 블라인드의 상태
 char mode = 'a'; // 모드 설정
-char ch;
+char ch, ch2;
+int buzPin = 10;
+unsigned long previousMillis = 0;
 
 int pos = 0;
 
@@ -17,6 +19,7 @@ void setup() {
   Serial.begin(9600);
   blind.attach(servo);
   blind.write(0);
+  pinMode(buzPin, OUTPUT);
 }
 
 void loop() {
@@ -24,7 +27,10 @@ void loop() {
     char tmp = Serial.read();
     if      (tmp == 'a' || tmp == 'A') mode = 'a';
     else if (tmp == 'b' || tmp == 'B') mode = 'b';
-    else ch = tmp;
+    else {
+      ch = tmp;
+      ch2 = tmp;
+    }
     
     Serial.print("mode");
     Serial.println(mode);
@@ -46,6 +52,17 @@ void loop() {
     else if(ch == '4') pos = 120;
     else if(ch == '5') pos = 150;
     else if(ch == '6') pos = 180;
+
+    unsigned long currentMillis = millis();
+    if (ch2 == '9'){
+      previousMillis = currentMillis;
+      digitalWrite(buzPin,HIGH);
+      ch2 = 0;
+    }
+    if (currentMillis - previousMillis >= 1000){
+      digitalWrite(buzPin,LOW);
+    }
+      
     
     ch = 0;
     blind.write(pos);
@@ -66,8 +83,8 @@ void autoControl(){
 //  Serial.println(cdsVal);
   
   //빛이 밝아지면
-  if (cdsVal < 400) {
-    if (flag == 0) {      //현재 블라인드가 올라가있으면     
+  if (cdsVal < 200) {
+    if (flag == 0 || flag == 2) {      //현재 블라인드가 올라가있으면     
       blind.write(180);   //블라인드를 내림
       flag = 1;           //블라인드가 내려가 있는 상태로 설정
       pos = 180;
@@ -76,9 +93,17 @@ void autoControl(){
     }
   }
 
+  else if (cdsVal >= 250 && cdsVal < 450){
+    if ( flag == 0 || flag == 1){
+      blind.write(90);
+      flag = 2;
+      pos = 90;
+    }
+  }
+
   //빛이 어두워지면
   else {
-    if (flag == 1) {      //블라인드가 내려가 있으면
+    if (flag == 1 || flag == 2) {      //블라인드가 내려가 있으면
       blind.write(0);     //블라인드를 올림
       flag = 0;           //블라인드가 올라가 있는 상태로 설정
       pos = 0;
